49290adb75c98bdc7c39d5d50abb5c30faceac2c
Sergej Mihno <bunkerche@gmail.com>
2020-01-27 14:26:32 +0300

    Add project files.

---
+++ Lib_1951/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_1951
+{
+    public class Class1
+    {
+    }
+}
